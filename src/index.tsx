61a7ae2401a1d346eb42997e4a236023b070c6d7
Bob Gardner <bob.hn.gardner@gmail.com>
2018-07-16 20:41:08 -0700

    Setup tutorial for local development

---
+++ src/index.tsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
+    public render() {
+        return (
+            <button className="square">
+                {/* TODO */}
+            </button>
+        );
+    }
+}
+
+// tslint:disable-next-line:max-classes-per-file
+class Board extends React.Component {
+    public render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+
+    private renderSquare(i: number) {
+        return <Square />;
+    }
+}
+
+// tslint:disable-next-line:max-classes-per-file
+class Game extends React.Component {
+    public render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
